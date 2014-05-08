daea6052517e68864e52ddc1ab78a190b1f01a71
piif <christian_lefebvre@laposte.net>
2014-05-08 12:26:58 +0200

    tests ...

--- examples/test_events.ino
+++ examples/test_events.ino
@@ -1,3 +1,11 @@
+
+//A VOIR :
+//http://forum.arduino.cc/index.php?topic=100906.msg756700#msg756700
+//notamment la partie sur le watchdog
+//
+//http://forum.arduino.cc/index.php?topic=100906.0
+//explication sur les interruptions pour tous les inputs
+
 #include <Arduino.h>
 #include <avr/sleep.h>
 
@@ -24,30 +32,26 @@ void button2(byte detail, void *data) {
 	bool b = digitalRead(3);
 	if (b != b3) {
 		b3 = b;
-		Serial.print("B3 ");
-		Serial.println(b3);
 	} else {
 		bool b = digitalRead(4);
 		if (b != b4) {
 			b4 = b;
-			Serial.print("B4 ");
-			Serial.println(b4);
 		} else {
 			bool b = digitalRead(5);
 			if (b != b5) {
 				b5 = b;
-				Serial.print("B5 ");
-				Serial.println(b5);
 			} else {
 				Serial.println("?");
 			}
 		}
 	}
-	Serial.flush();
+	digitalWrite(7, b3);
+	digitalWrite(6, b4);
+	digitalWrite(8, b5);
+	digitalWrite(12, HIGH);
 }
 void button(byte detail, void *data) {
-	Serial.print("button ...");
-	Serial.flush();
+	digitalWrite(12, LOW);
 	Scheduler.registerTimeout(1, button2, 0);
 }
 
@@ -58,29 +62,30 @@ void setup() {
 
 	for (int i = 6; i <= 12; i++) {
 		pinMode(i, OUTPUT);
-		digitalWrite(i, LOW);
+		digitalWrite(i, HIGH);
 	}
 	pinMode(13, OUTPUT);
 	digitalWrite(13, HIGH);
 
 	Scheduler.begin();
 	Events::eventHandler *eh;
-	eh = Scheduler.registerInterval(300, 30, rollStep, 0);
-	if (eh == 0) {
-		Serial.println("timer 1 failed");
-	}
+//	eh = Scheduler.registerInterval(300, 30, rollStep, 0);
+//	if (eh == 0) {
+//		Serial.println("timer 1 failed");
+//	}
 	eh = Scheduler.registerInterval(150, -1, blink, 0);
 	if (eh == 0) {
 		Serial.println("timer 2 failed");
 	}
-	eh = Scheduler.registerInput(2, CHANGE, button, 0);
-	if (eh == 0) {
-		Serial.println("input failed");
-	}
+//	eh = Scheduler.registerInput(2, CHANGE, button, 0);
+//	if (eh == 0) {
+//		Serial.println("input failed");
+//	}
 }
 
 void loop() {
 	Scheduler.waitNext(SLEEP_MODE_IDLE);
 // TODO : WHY DOESN'T IT WORK WITH PWR_SAVE !!!
 //	Scheduler.waitNext(SLEEP_MODE_PWR_SAVE);
+//	Scheduler.waitNext(SLEEP_MODE_ADC);
 }
